d51f7b4e6db31162338c2a07099e84786e4f6b4a
Keepingitneil <neildwyer1991@gmail.com>
2025-04-30 21:07:30 -0700

    missed the save

--- app/example-with-usage-token/actions.ts
+++ app/example-with-usage-token/actions.ts
@@ -12,15 +12,8 @@ export async function createTokenAndContext() {
         ]
     })
 
-<<<<<<< HEAD
-    const contextRes = await axios.post("https://api.gabber.dev/v1/llm/context", contextBody, { headers: { "x-api-key": process.env.GABBER_API_KEY } });
-    const usageTokenRes = await axios.post("https://api.gabber.dev/v1/usage/token", { human: "some-user-id", ttl_seconds: 3600 * 24 }, { headers: { "x-api-key": process.env.GABBER_API_KEY } });
-    return { token: usageTokenRes.data.token, context: contextRes.data.id }
-}
-=======
     const human = "some-user-id"
     const contextRes = await axios.post("https://api.gabber.dev/v1/llm/context", contextBody, { headers: { "x-api-key": process.env.GABBER_API_KEY, "x-human-id": human } });
     const usageTokenRes = await axios.post("https://api.gabber.dev/v1/usage/token", { human, ttl_seconds: 3600 * 24 }, { headers: { "x-api-key": process.env.GABBER_API_KEY } });
     return { token: usageTokenRes.data.token, context: contextRes.data.id }
 }
->>>>>>> a123166 (misc fixes)
