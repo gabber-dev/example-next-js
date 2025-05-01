962ae3f789f5f9e77378f33daecbdf41c6b3bcd9
Keepingitneil <neildwyer1991@gmail.com>
2025-04-30 21:06:25 -0700

    misc fixes

--- app/example-with-usage-token/page.tsx
+++ app/example-with-usage-token/page.tsx
@@ -1,5 +1,5 @@
 "use client"
-import { RealtimeSessionEngineProvider } from "gabber-client-react";
+import { RealtimeSessionEngineProvider } from "gabber-client-react"
 import { useCallback, useState } from "react";
 import { App } from "@/components/App";
 import { createTokenAndContext } from "./actions";
@@ -8,16 +8,15 @@ const GABBER_VOICE = "626c3b02-2d2a-4a93-b3e7-be35fd2b95cd"
 const GABBER_LLM = "21892bb9-9809-4b6f-8c3e-e40093069f04"
 
 export default function Home() {
-    const [token, setToken] = useState<string | undefined>(undefined);
-    const [context, setContext] = useState<string | undefined>(undefined);
+    const [details, setDetails] = useState<{ token: string, context: string } | undefined>(undefined);
 
     const generateConnectionDetails = useCallback(async () => {
         const { token, context: contextId } = await createTokenAndContext()
-        setToken(token)
-        setContext(contextId)
+        console.log("Token: ", token, "Context: ", contextId)
+        setDetails({ token, context: contextId })
     }, [])
 
-    if (!token) {
+    if (!details) {
         return (
             <div className="w-screen h-screen flex items-center justify-center bg-gray-50">
                 <button className="p-2 bg-blue-400 rounded font-bold" onClick={async () => {
@@ -27,8 +26,10 @@ export default function Home() {
         );
     }
 
+    console.log("Details: ", details)
+
     return <RealtimeSessionEngineProvider connectionOpts={{
-        token, config:
+        token: details.token, config:
         {
             "general": {
                 "save_messages": true
@@ -40,7 +41,7 @@ export default function Home() {
             "generative": {
                 "llm": GABBER_LLM,
                 "voice_override": GABBER_VOICE,
-                "context": context,
+                "context": details.context,
             },
             "output": {
                 "stream_transcript": true,
